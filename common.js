9d7dc7d2e474028c546b739153ed4ba2dd65adf0
tilde-ad <tilde.davidsen@gmail.com>
2025-01-06 15:22:50 +0100

    Påbörjat den gemensamma javascript och fortsatt css
    Har påbörjat den gemensamma javascripten, skapat div för input och knapp samt "How many numbers in grid"-text. Även skapat en funktion som returnerar ett slumpmässigt tal mellan 1 och 100.

--- common.js
+++ common.js
@@ -1,6 +1,35 @@
+const body = document.querySelector("body");
 const homebutton = document.querySelector(".home-button");
 const a = document.createElement("a");
+const numbers = document.createElement("div");
+const input = document.createElement("input");
+const h2 = document.createElement("h2");
+const createButton = document.createElement("button");
+const script = document.querySelector("script");
+
+let inputText = input.value;
+
 homebutton.appendChild(a);
 a.setAttribute("class", "home-links");
-a.setAttribute("href", "index.html");
+a.setAttribute("href", "../index.html");
 a.textContent = "Home"
+body.insertBefore(numbers, script);
+numbers.appendChild(h2);
+numbers.appendChild(input);
+h2.textContent = "How many numbers in the grid?";
+numbers.append(createButton);
+createButton.textContent = "Create";
+numbers.setAttribute("id", "numbers");
+
+
+console.log(inputText);
+
+
+function randomNumber() {
+    Number = Math.ceil(100 * Math.random());
+    return Number;
+}
+
+console.log(randomNumber());
+
+
